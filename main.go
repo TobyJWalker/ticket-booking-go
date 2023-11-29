979a12328c26bae122819721767017f7a99edb21
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-29 22:06:30 +0000

    Added user input, displayed data types

--- main.go
+++ main.go
@@ -6,15 +6,34 @@ import "fmt" // Import fmt package for printing etc
 func main() {
 
 	// Initialise globals
-	const CONFERENCE_NAME string = "Go Conference"
-	const CONFERENCE_TICKETS int = 50
+	const CONFERENCE_NAME = "Go Conference"
+	const CONFERENCE_TICKETS = 50
 
 	// Initialise variables
-	var remainingTickets int = CONFERENCE_TICKETS
+	var remainingTickets = CONFERENCE_TICKETS
+
+	// Display value data types
+	fmt.Printf("\n\nConference name is of type: %T\n", CONFERENCE_NAME)
+	fmt.Printf("Conference tickets is of type: %T\n", CONFERENCE_TICKETS)
+	fmt.Printf("Remaining tickets is of type: %T\n\n", remainingTickets)
 
 	// Display welcome message and ticket information
 	fmt.Printf("Welcome to %s booking application.\n", CONFERENCE_NAME)
 	fmt.Printf("There are %d total spaces available and %d remaining.\n", CONFERENCE_TICKETS, remainingTickets)
-	fmt.Println("Get your tickets here to attend our conference!")
+	fmt.Println("Get your tickets here to attend our conference!\n")
+
+	// Initialise user input variable
+	var userName string = ""
+	var userTickets int = 0
+
+	// Get user input
+	fmt.Print("Please enter your name: ")
+	fmt.Scanln(&userName)
+
+	fmt.Print("Please enter the number of tickets you would like to purchase: ")
+	fmt.Scanln(&userTickets)
+
+	// Display user input
+	fmt.Printf("Thank you %s, you have purchased %d tickets.\n", userName, userTickets)
 
 }
