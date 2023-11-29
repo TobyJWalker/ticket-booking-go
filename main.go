1b05c04cccb86e96a840fa2099e2b453bf4a6385
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-29 21:51:53 +0000

    added variables, constants and print formatting

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import "fmt" // Import fmt package for printing etc
 
 // Main function
 func main() {
-	fmt.Println("Welcome to our conference booking application.")
+
+	// Initialise globals
+	const CONFERENCE_NAME string = "Go Conference"
+	const CONFERENCE_TICKETS int = 50
+
+	// Initialise variables
+	var remainingTickets int = CONFERENCE_TICKETS
+
+	// Display welcome message and ticket information
+	fmt.Printf("Welcome to %s booking application.\n", CONFERENCE_NAME)
+	fmt.Printf("There are %d total spaces available and %d remaining.\n", CONFERENCE_TICKETS, remainingTickets)
 	fmt.Println("Get your tickets here to attend our conference!")
+
 }
