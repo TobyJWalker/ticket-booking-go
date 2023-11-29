1499c32b2bb98d246b3d6aec388ea8c893fea513
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-29 21:36:21 +0000

    Print welcome message

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt" // Import fmt package for printing etc
 
 // Main function
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to our conference booking application.")
+	fmt.Println("Get your tickets here to attend our conference!")
 }
