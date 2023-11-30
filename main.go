c66a3e8935d022e40431bd1a7bae34b492d15cdf
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-30 13:20:25 +0000

    Removed some funcs to shared file

--- main.go
+++ main.go
@@ -84,46 +84,6 @@ func getUserDetails() (string, string, string, int) {
 	return firstName, lastName, emailAddress, userTickets
 }
 
-// Function to validate user input, tell them what is wrong and return a boolean
-func isValidInput(firstName string, lastName string, emailAddress string, userTickets int) bool {
-
-	// Name validation, must be at least 2 characters long
-	isInvalidName := len(firstName) < 2 || len(lastName) < 2
-
-	if isInvalidName {
-		fmt.Printf("\nSorry, your name must be at least 2 characters long.\n")
-		fmt.Printf("Please try again.\n")
-		return false
-	}
-
-	// Email validation, must contain an @ symbol
-	isInvalidEmail := !strings.Contains(emailAddress, "@")
-
-	if isInvalidEmail {
-		fmt.Printf("\nSorry, your email address must contain an @ symbol.\n")
-		fmt.Printf("Please try again.\n")
-		return false
-	}
-
-	// Ticket validation, must be at least 1 ticket
-	isInvalidTicketNumber := userTickets < 1
-
-	if isInvalidTicketNumber {
-		fmt.Printf("\nSorry, you must book at least 1 ticket.\n")
-		fmt.Printf("Please try again.\n")
-		return false
-	}
-
-	// Check if there are enough tickets remaining
-	if uint(userTickets) > remainingTickets {
-		fmt.Printf("\nSorry, there are only %d tickets remaining.\n", remainingTickets)
-		fmt.Printf("Please try again.\n")
-		return false
-	}
-
-	return true
-}
-
 // Function to get remaining tickets
 func calculateRemainingTickets(userTickets int) {
 	remainingTickets = remainingTickets - uint(userTickets)
@@ -138,23 +98,4 @@ func processBooking(firstName string, lastName string, emailAddress string, user
 	// Display user input
 	fmt.Printf("\nThank you %s, you have booked %d tickets.\n", firstName, userTickets)
 	fmt.Printf("A confirmation email has been sent to %s.\n", emailAddress)
-}
-
-// Function to display bookings
-func displayBookings() {
-
-	// Get a list of the first names of bookings (for privacy)
-	firstNames := []string{}
-
-	for _, booking := range bookings { // _ is used to ignore the index value given with range
-
-		// Get the first name of the booking and add to slice
-		var splitNames = strings.Split(booking, " ")
-		firstNames = append(firstNames, splitNames[0])
-		
-	}
-
-	// Display list of booking first names
-	fmt.Println("\nBookings:")
-	fmt.Printf("%s\n", firstNames)
 }
